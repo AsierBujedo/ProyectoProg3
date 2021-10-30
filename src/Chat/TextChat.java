b88d06b711efe1f35a548c7b5ee37a7ea5826191
AsierBujedo <asier.bujedo@opendeusto.es>
2021-10-30 22:42:04 +0200

    Chat fixed

--- src/Chat/TextChat.java
+++ src/Chat/TextChat.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 import java.net.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.awt.event.*;
 
@@ -18,7 +20,7 @@ public class TextChat {
 	JFrame frame = new JFrame();
 	JTextArea chat = new JTextArea();
 	JTextField text = new JTextField();
-	boolean connected;
+	static boolean connected;
 
 	public void InitChat(int PORT) {
 
@@ -29,13 +31,18 @@ public class TextChat {
 			socket = serversocket.accept(); // Aceptamos la conexión e iniciamos el servidor
 			chat.append("[SYSTEM]   Connected to " + socket.getInetAddress() + "\n");
 			connected = true;
-			// Obtenemos el método de entrada de datos e interacción con el exterior
+			// Obtenemos el método de entrada y salida de datos.
 			inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			Outputpw = new PrintWriter(socket.getOutputStream(), true);
 			while (connected) {
-				chat.append("[INVITADO]   " + inputReader.readLine() + "\n");
+				chat.append("[REMOTO]   " + inputReader.readLine() + "\n");
+			}
+			while (true) {
 				if (!connected) {
 					socket.close();
+					serversocket.close();
+					Outputpw.close();
+					inputReader.close();
 				}
 			}
 		} catch (IOException ex) {
@@ -53,15 +60,29 @@ public class TextChat {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-					chat.append("[Tú]   " + text.getText() + "\n");
-					if (connected) {
-						Outputpw.println(text.getText());
+					if (text.getText().equals("!clear")) {
+						chat.setText("");
+						text.setText("");
+					} else if (text.getText().equals("!time")) {
+						DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+						chat.append("Current time: " + dtf.format(LocalDateTime.now()) + "\n");
+						text.setText("");
+					} else if (text.getText().equals("!close")) {
+						frame.dispose();
+						Outputpw.println("Connection closed");
+						connected = false;
+					} else {
+						chat.append("[Tú]   " + text.getText() + "\n");
+						if (connected) {
+							Outputpw.println(text.getText());
+						}
+						text.setText("");
 					}
-					text.setText("");
 				}
 
 			}
 		});
+		
 
 		frame.setTitle("Chat del juego");
 		frame.setSize(300, 350);
--- src/Chat/TextChatClient.java
+++ src/Chat/TextChatClient.java
@@ -1,12 +1,16 @@
 package Chat;
 
-import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
-import java.net.*;
+import java.awt.*;
 import javax.swing.*;
+import java.net.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.awt.event.*;
 
 public class TextChatClient {
+	ServerSocket serversocket = null;
+	Socket socket = null;
 	InputStream in;
 	OutputStream os;
 	PrintWriter Outputpw;
@@ -14,22 +18,31 @@ public class TextChatClient {
 	JFrame frame = new JFrame();
 	JTextArea chat = new JTextArea();
 	JTextField text = new JTextField();
-	Socket socket = null;
-	boolean connected;
+	static boolean connected;
 
 	public void InitChat(String HOST, int PORT) {
+
 		ChatWindow();
+
 		try {
 			socket = new Socket(HOST, PORT);
 			chat.append("[SYSTEM]   Connected to " + socket.getInetAddress() + "\n");
 			connected = true;
+			// Obtenemos el método de entrada de datos e interacción con el exterior
 			inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			Outputpw = new PrintWriter(socket.getOutputStream(), true);
 			while (connected) {
-				chat.append("[HOST]   " + inputReader.readLine() + "\n");
+				chat.append("[REMOTO]   " + inputReader.readLine() + "\n");
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			while (true) {
+				if (!connected) {
+					socket.close();
+					Outputpw.close();
+					inputReader.close();
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
 
 	}
@@ -43,11 +56,24 @@ public class TextChatClient {
 			@Override
 			public void keyTyped(KeyEvent e) {
 				if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-					chat.append("[Tú]   " + text.getText() + "\n");
-					if (connected) {
-						Outputpw.write(text.getText() + "\n");
+					if (text.getText().equals("!clear")) {
+						chat.setText("");
+						text.setText("");
+					} else if (text.getText().equals("!time")) {
+						DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+						chat.append("Current time: " + dtf.format(LocalDateTime.now()) + "\n");
+						text.setText("");
+					} else if (text.getText().equals("!close")) {
+						frame.dispose();
+						Outputpw.println("Connection closed");
+						connected = false;
+					} else {
+						chat.append("[Tú]   " + text.getText() + "\n");
+						if (connected) {
+							Outputpw.println(text.getText());
+						}
+						text.setText("");
 					}
-					text.setText("");
 				}
 
 			}
