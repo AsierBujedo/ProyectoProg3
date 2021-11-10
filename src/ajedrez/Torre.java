d94187714002617286e82a55e3546c630d564cfb
MikelLambarri <mikel.lambarri@opendeusto.es>
2021-11-10 19:27:38 +0100

    Movimientos de la torre

--- src/ajedrez/Torre.java
+++ src/ajedrez/Torre.java
@@ -2,6 +2,7 @@ package ajedrez;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Torre extends Pieza{
 
@@ -14,7 +15,32 @@ public class Torre extends Pieza{
 		ArrayList<Cuadrado> movimientos=new ArrayList<Cuadrado>();
 		int x=cuadradoActual.getX();
 		int y=cuadradoActual.getY();
-		return null;
+		for (int i = x+1; i < tablero.getHeight(); i++) {//movimiento a x positivo.
+			if(tablero.GetCuadrados()[y][i].estaOcupado()==false) {
+				movimientos.add(tablero.GetCuadrados()[y][i]);
+			}else {break;
+			}
+		}
+		for (int i = x-1; i < 0; i--) {//movimiento a x negativo
+			if(tablero.GetCuadrados()[y][i].estaOcupado()==false) {
+				movimientos.add(tablero.GetCuadrados()[y][i]);
+			}else {break;
+			}
+		}
+		for (int i = y+1; i < tablero.getWidth(); i++) {//movimiento a y positivo
+			if(tablero.GetCuadrados()[i][x].estaOcupado()==false) {
+				movimientos.add(tablero.GetCuadrados()[y][i]);
+			}else {break;
+			}
+		}
+		for (int i = y-1; i < 0; i--) {//movimiento a y negativo
+			if(tablero.GetCuadrados()[i][x].estaOcupado()==false) {
+				movimientos.add(tablero.GetCuadrados()[y][i]);
+			}else {break;
+			}
+		}
+
+		return movimientos;
 	}
 
 }
