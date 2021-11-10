233847c6bd9a1d87dfd7152dc55b68f44f2223d9
Mikel <Mikel@192.168.1.38>
2021-11-10 18:08:57 +0100

    Movimientos para el peón

--- src/ajedrez/Peon.java
+++ src/ajedrez/Peon.java
@@ -4,15 +4,53 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Peon extends Pieza{
+	boolean seHaMovido;
 
-	public Peon(int color, Cuadrado cuadradoActual, String img) {
+	public Peon(int color, Cuadrado cuadradoActual, String img, boolean seHaMovido) {
 		super(color, cuadradoActual, img);
+		this.seHaMovido=seHaMovido;
 	}
 
 	@Override
 	public ArrayList<Cuadrado> getMovimientosLegales(Tablero tablero) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Cuadrado> movimientos=new ArrayList<Cuadrado>();
+		int x=cuadradoActual.getX();
+		int y=cuadradoActual.getY();
+		if(this.color==0) {//casos con los peones abajo (están "subiendo")
+			if(tablero.GetCuadrados()[y+1][x].estaOcupado()==false) {//casilla de enfrente
+				movimientos.add(tablero.GetCuadrados()[y+1][x]);
+			}
+			if(tablero.GetCuadrados()[y+1][x+1].estaOcupado()==true) {//casilla diagonal derecha, para comer pieza
+				movimientos.add(tablero.GetCuadrados()[y+1][x+1]);
+			}
+			if(tablero.GetCuadrados()[y+1][x-1].estaOcupado()==true) {//casilla diagonal izquierda, para comer pieza
+				movimientos.add(tablero.GetCuadrados()[y+1][x-1]);
+			}
+			if(this.seHaMovido==false) {
+				if(tablero.GetCuadrados()[y+2][x].estaOcupado()==false) {//casilla dos enfrente, solo si no se ha movido antes
+					movimientos.add(tablero.GetCuadrados()[y+1][x]);
+				}
+				
+			}
+		}
+		if(this.color==0) {//casos con los peones arriba (están "bajando")
+			if(tablero.GetCuadrados()[y-1][x].estaOcupado()==false) {//casilla de enfrente
+				movimientos.add(tablero.GetCuadrados()[y+1][x]);
+			}
+			if(tablero.GetCuadrados()[y-1][x+1].estaOcupado()==true) {//casilla diagonal derecha, para comer pieza
+				movimientos.add(tablero.GetCuadrados()[y+1][x+1]);
+			}
+			if(tablero.GetCuadrados()[y-1][x-1].estaOcupado()==true) {//casilla diagonal izquierda, para comer pieza
+				movimientos.add(tablero.GetCuadrados()[y+1][x-1]);
+			}
+			if(this.seHaMovido==false) {
+				if(tablero.GetCuadrados()[y-2][x].estaOcupado()==false) {//casilla dos enfrente, solo si no se ha movido antes
+					movimientos.add(tablero.GetCuadrados()[y+1][x]);
+				}
+				
+			}
+		}
+		return movimientos;
 	}
 
 }
